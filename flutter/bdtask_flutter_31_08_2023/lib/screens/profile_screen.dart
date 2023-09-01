3cb998cb4a61d8951b2d0e822aaf10945b7179d5
Shahed Noor <shahedshishir@proton.me>
2023-09-01 22:15:47 +0600

    profile page updated

--- flutter/bdtask_flutter_31_08_2023/lib/screens/profile_screen.dart
+++ flutter/bdtask_flutter_31_08_2023/lib/screens/profile_screen.dart
@@ -9,33 +9,97 @@ class ProfileScreen extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.amber),
       home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 25),
-              CircleAvatar(
-                radius: 75,
-                backgroundImage: AssetImage("images/profile_image.png")
-              ),
-              SizedBox(height: 10),
-              Text("Shahed Noor", style: TextStyle(fontSize: 20)),
-              SizedBox(height: 10),
-              Text("Always ready to explore new things!"),
-              SizedBox(height: 10),
-              Container(
-                width: 150,
-                height: 40,
-                child: TextButton(
-                  onPressed: () {
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.black),
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                  ),
-                  child: Text("Edit Profile"),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 25),
+                CircleAvatar(
+                  radius: 75,
+                  backgroundImage: AssetImage("images/profile_image.png")
                 ),
-              ),
-            ],
+                SizedBox(height: 10),
+                Text("Shahed Noor", style: TextStyle(fontSize: 20)),
+                SizedBox(height: 10),
+                Text("Always ready to explore new things!"),
+                SizedBox(height: 10),
+                Container(
+                  width: 150,
+                  height: 40,
+                  child: TextButton(
+                    onPressed: () {
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.black),
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                    ),
+                    child: Text("Edit Profile"),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ListTile(
+                  leading: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Color.fromARGB(255, 201, 129, 213).withOpacity(0.1)
+                    ),
+                    child: Icon(Icons.settings),
+                  ),
+                  title: Text("Settings"),
+                ),
+                ListTile(
+                  leading: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Color.fromARGB(255, 201, 129, 213).withOpacity(0.1)
+                    ),
+                    child: Icon(Icons.blinds_closed_sharp),
+                  ),
+                  title: Text("Billing Details"),
+                ),
+                ListTile(
+                  leading: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Color.fromARGB(255, 201, 129, 213).withOpacity(0.1)
+                    ),
+                    child: Icon(Icons.person_add_alt),
+                  ),
+                  title: Text("User Management"),
+                ),
+                ListTile(
+                  leading: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Color.fromARGB(255, 201, 129, 213).withOpacity(0.1)
+                    ),
+                    child: Icon(Icons.info),
+                  ),
+                  title: Text("Information"),
+                ),
+                ListTile(
+                  leading: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Color.fromARGB(255, 201, 129, 213).withOpacity(0.1)
+                    ),
+                    child: Icon(Icons.logout),
+                  ),
+                  title: Text("Logout"),
+                  textColor: Colors.red,
+                ),
+              ],
+            ),
           ),
         ),
       ),
